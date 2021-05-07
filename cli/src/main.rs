c5f0bd579d19ef1af07663c0683056ac2f4c43be
RogueArt <altoraries@gmail.com>
2021-05-06 22:37:48 -0700

    Set up greet message for Pom CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,11 +1,35 @@
-pub mod user_io;
-use user_io::get_num_seconds;
+use std::io::{stdout, Write};
 
-pub mod timer;
-use timer::Timer;
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, Result,
+    event,
+};
 
 fn main() {
-    let seconds = get_num_seconds();
-    let mut timer = Timer::new(seconds);
-    timer.start();
+  greet_new_user();
+}
+
+fn get_pause() {
+  
+}
+
+fn greet_new_user() {
+  execute!(
+    stdout(),
+    Print("🎉 Welcome to Pom!🎉\n"),
+    Print("It looks like this is the first time you're using this...\n"),
+    Print("Here's a quick-start:\n"),
+
+    SetForegroundColor(Color::Blue),
+    Print("📖 Ctrl + H -> Help\n"),
+    Print("⏸  Ctrl + P -> Pause/Settings\n"),
+    Print("⏲  Ctrl + T -> Timer\n"),
+    Print("🔄 Ctrl + R -> Reset Timer\n"),
+    ResetColor,
+
+    Print("You can start the timer when you're ready!"),
+    ResetColor,
+  );
 }
---
+++ cli/src/main_old.rs
@@ -0,0 +1,12 @@
+pub mod user_io;
+use user_io::get_num_seconds;
+
+pub mod timer;
+use timer::Timer;
+
+fn main() {
+    
+    // let seconds = get_num_seconds();
+    // let mut timer = Timer::new(seconds);
+    // timer.start();
+}
