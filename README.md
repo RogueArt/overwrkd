a9806f0607ec047dfb001cbaca8fb4fb4130156a
RogueArt <altoraries@gmail.com>
2021-06-29 22:33:57 -0700

    Updated README

--- README.md
+++ README.md
@@ -2,18 +2,17 @@
 
 Pom is a customizable pomodoro timer that comes in various flavors. It can be used as either a CLI tool or as a desktop GUI tool and it's platform compatible with all major operating systems. More information will be added as Pom reaches version 1.0.0.
 
-# Inspiration
-
 Pom is meant to be an improvement over the current loop timer I'm using. See [here](https://github.com/RogueArt/loop-timer/) for my **Windows only** loop timer.
 
 # Getting Started
 
 Downloadable executables for all major platforms will be added once Pom reaches version 1.0.0.
 
-For now, you can build this yourself by installing Rust as shown [here](https://www.rust-lang.org/tools/install).
+To build it yourself, follow these steps:
+1. Install Rust from the [official Rust lang website](https://www.rust-lang.org/tools/install).
+2. Clone this repository using `https://github.com/RogueArt/pom.git`
+3. Build both executables for your current platform with `cargo build --release`. 
 
-Then you can clone this repository and build it via the terminal by running from the root of the directory:
+Executables (cli.exe and gui.exe) can be found under `./target/debug`. This will take up to a minute depending on the speed of your computer.
 
-> cargo build --release
-
-Cargo will automatically install dependencies and compile it under /target.
+Note: Cargo, the package manager for Rust, will automatically install and compile any needed dependencies.
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,14 +1,83 @@
-use std::io::{stdout, Write};
+use std::{io::{stdout, Write}, time::Duration};
 
-use crossterm::{
-    execute,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand, Result,
-    event,
+use crossterm::{ExecutableCommand, 
+  Result,
+   event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers}, 
+   execute, 
+   style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor}
+
 };
+
+const HELP: Event = Event::Key(KeyEvent {
+  code: KeyCode::Char('h'),
+  modifiers: KeyModifiers::CONTROL,
+});
+
+const PAUSE: Event = Event::Key(KeyEvent {
+  code: KeyCode::Char('o'),
+  modifiers: KeyModifiers::CONTROL,
+});
+
+const TIMER: Event = Event::Key(KeyEvent {
+  code: KeyCode::Char('t'),
+  modifiers: KeyModifiers::CONTROL,
+});
+
+const RESET: Event = Event::Key(KeyEvent {
+  code: KeyCode::Char('r'),
+  modifiers: KeyModifiers::CONTROL,
+});
+
+fn print_events() -> Result<()> {
+  loop {
+    if poll(Duration::from_millis(1_000))? {
+      let event = read()?;
+
+      println!("Event::{:?}\r", event);
+      match_event(event);
+
+      if event == Event::Key(KeyCode::Esc.into()) {
+        break;
+      }
+    }
+  }
+
+  Ok(())
+}
 
 fn main() {
   greet_new_user();
+  print_events();
+
+
+  // let x = KeyEvent::from(KeyCode::Char('c'));
+  // let help = KeyEvent::new(KeyCode::Char('c'), KeyModifiers::CONTROL);
+  // let pause = KeyEvent::new(KeyCode::Char('c'), KeyModifiers::CONTROL);
+  // let timer = KeyEvent::new(KeyCode::Char('c'), KeyModifiers::CONTROL);
+  // let reset = KeyEvent::new(KeyCode::Char('c'), KeyModifiers::CONTROL);
+
+  // let mut sync_stdin = input.read_sync();
+
+  // loop {
+  //   if let Some(event) = sync_stdin.next() {
+  //     match event {
+  //       KeyEvent::Char('h') => println!("Help"),
+  //       InputEvent::Keyboard(KeyEvent::Ctrl('p')) => println!("Pause"),
+  //       InputEvent::Keyboard(KeyEvent::Ctrl('t')) => println!("Timer"),
+  //       InputEvent::Keyboard(KeyEvent::Ctrl('r')) => println!("Reset"),
+  //     }
+  //   }
+  // }
+}
+
+fn match_event(event: Event) {
+  match event {
+      HELP => println!("Help"),
+      PAUSE => println!("Pause"),
+      TIMER => println!("Timer"),
+      RESET => println!("Reset"),
+      _ => {},
+  }
 }
 
 fn get_pause() {
